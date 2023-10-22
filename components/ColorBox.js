af38816061b81bda57b6968555bc094f49831dd2
Krystian <krystian1wojtowicz@gmail.com>
2023-10-22 20:56:33 +0200

    list and nav

---
+++ components/ColorBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ColorBox = ({ colorName, hexCode }) => {
+  const boxColor = {
+    backgroundColor: hexCode,
+  };
+  const textColor = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+  return (
+    <View style={[styles.box, boxColor]}>
+      <Text style={[styles.boxText, textColor]}>
+        {colorName} {hexCode}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  boxText: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export default ColorBox;
